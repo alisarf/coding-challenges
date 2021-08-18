ea6986891723b1f9bcf82fff3044628c61280197
alisarf <alisarfaresboulos@gmail.com>
2021-08-18 06:05:36 -0700

    JS steps created

---
+++ sunnyside-agency-landing-page-main/js/nav-hamburger.js
@@ -0,0 +1,13 @@
+//HAMBURGER NAV
+
+//1. CACHE the nav wrapper
+
+//2. Set it to hidden at certain View width in css
+
+//3. Add a hamburger object to screen with js
+
+//4. Style hamburger object in css when small view
+
+//5. add event listener on click of hamburger
+
+//6. toggle between the visibility of hamburger and the options?
