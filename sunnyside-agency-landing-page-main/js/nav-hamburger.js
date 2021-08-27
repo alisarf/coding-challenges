9c1013255ff42b5662254f1e95c1c661cf66282a
alisarf <alisarfaresboulos@gmail.com>
2021-08-27 12:28:16 -0700

    Sunnyside Completed Challenge

--- sunnyside-agency-landing-page-main/js/nav-hamburger.js
+++ sunnyside-agency-landing-page-main/js/nav-hamburger.js
@@ -1,13 +1,19 @@
 //HAMBURGER NAV
+const menuBtn = document.querySelector('.menu-btn');
+const menuItems = document.querySelector('.nav-content');
+const triangle = document.querySelector('.triangle');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuItems.classList.add('open');
+        triangle.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuItems.classList.remove('open');
+        triangle.classList.remove('open');
+        menuOpen = false;
+    }
+}, true);
 
-//1. CACHE the nav wrapper
-
-//2. Set it to hidden at certain View width in css
-
-//3. Add a hamburger object to screen with js
-
-//4. Style hamburger object in css when small view
-
-//5. add event listener on click of hamburger
-
-//6. toggle between the visibility of hamburger and the options?
